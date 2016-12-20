277900ad15237032c29f5b8ea957d1a4b9368460
ChrisJamesC <christopher.chiche@gmail.com>
2016-12-20 10:21:27 +0100

    Install Open Weather Map client

--- src/main/java/com/chris/training/App.java
+++ src/main/java/com/chris/training/App.java
@@ -1,13 +1,15 @@
 package com.chris.training;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+        OwmJapis.singleImport();
         System.out.println( "Hello World!" );
     }
 }
